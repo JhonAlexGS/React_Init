4b673c53e3253bc136e5e4fcfb2f2018dc475fc6
Jhon Alexander Garcia Sierra <jhon@booklick.co>
2024-06-27 16:20:59 -0500

    Se recorrio y se añadio una lista en los elementos del componente

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import { TodoCounter } from './TodoCounter';
 import { TodoSearch } from './TodoSearch';
 import { TodoList } from './TodoList';
-import logo from './platzi.webp';
+// import logo from './platzi.webp';
 import './App.css';
 import { TodoItem } from './TodoItem';
 import { CreateTodoButton } from './CreateTodoButton';
 import React from 'react';
+
+const defaultTodos = [
+  { text: 'Cortar cebolla', completed: false },
+  { text: 'Tomarel crso e Intro a React.js', completed: false },
+  { text: 'Llorar con la llorona', completed: false },
+  { text: 'ALALALLALA', completed: false },
+  { text: 'ALALALLALA', completed: false }
+]
 
 function App() {
   return (
@@ -22,16 +30,20 @@ function App() {
       {/* Lista de Todos */}
       <TodoList>
 
-        <TodoItem/>
-        <TodoItem/>
-        <TodoItem/>
+        {/* {defaultTodos} */}
+        {defaultTodos.map(todo =>(
+          <TodoItem key={todo.text} 
+          text={todo.text}
+          completed={todo.completed}
+          />
+        ))}
 
       </TodoList>
 
       {/* Boton que permite crear la funconalidad de crear Todos */}
       <CreateTodoButton />
 
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edita el archivo <code>src/App.js</code> y guarda para recargar.
@@ -44,7 +56,7 @@ function App() {
         >
           Aprendamos con React
         </a>
-      </header>
+      </header> */}
     </React.Fragment>
   );
 }
