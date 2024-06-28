586be51e8950749973d3c1b23f0fb2b43b81dd10
Jhon Alexander Garcia Sierra <jhon@booklick.co>
2024-06-28 02:22:28 -0500

    CSS y Eventos
    Se añadio el CSS, se reconfiguro el archivo y se le añadio eventos para hacer click y obtener lo que se esta buscando por el input

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
 import { TodoCounter } from './TodoCounter';
 import { TodoSearch } from './TodoSearch';
 import { TodoList } from './TodoList';
-// import logo from './platzi.webp';
-import './App.css';
 import { TodoItem } from './TodoItem';
 import { CreateTodoButton } from './CreateTodoButton';
 import React from 'react';
 
 const defaultTodos = [
-  { text: 'Cortar cebolla', completed: false },
+  { text: 'Cortar cebolla', completed: true },
   { text: 'Tomarel crso e Intro a React.js', completed: false },
   { text: 'Llorar con la llorona', completed: false },
-  { text: 'ALALALLALA', completed: false },
   { text: 'ALALALLALA', completed: false }
 ]
 
@@ -21,8 +18,6 @@ function App() {
 
       {/* Completaste tanto de tantos componentes */}
       <TodoCounter completed={16} total={25} />
-      <TodoCounter completed={8} total={12} />
-      <TodoCounter completed={3} total={5} />
 
       {/* Campo qu  e nos deja filtrar Todos */}
       <TodoSearch />
@@ -43,20 +38,6 @@ function App() {
       {/* Boton que permite crear la funconalidad de crear Todos */}
       <CreateTodoButton />
 
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edita el archivo <code>src/App.js</code> y guarda para recargar.
-        </p>
-        <a
-          className="App-link"
-          href="https://platzi.com/reactjs"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Aprendamos con React
-        </a>
-      </header> */}
     </React.Fragment>
   );
 }
--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -1,7 +1,18 @@
-function TodoSearch(){
-    return (
-        <input placeholder="Cortar Cebolla"/>   
-    );
-  };
+import './TodoSearch.css';
 
-export {TodoSearch};
+function TodoSearch() {
+  return (
+    <input
+      placeholder="Cortar cebolla"
+      className="TodoSearch"
+      onChange={(event) => {
+        console.log('Escribiste en el TodoSearch');
+        console.log(event);
+        console.log(event.target);
+        console.log(event.target.value);
+      }}
+    />
+  );
+}
+
+export { TodoSearch };
