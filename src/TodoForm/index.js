1bd0579ca97c2007bf29efedee7600caf7ed0608
Jhon Alexander Garcia Sierra <jhon@booklick.co>
2024-07-10 09:42:39 -0500

    Se añadio la funcionalidad de agregar ToDos

--- src/TodoForm/index.js
+++ src/TodoForm/index.js
@@ -1,25 +1,53 @@
 import React from "react"
 import './TodoForm.css'
+import { TodoContext } from "../TodoContext";
 
 function TodoForm(){
+
+    const {
+        addTodo,
+        setOpenModal,
+    } = React.useContext(TodoContext);
+
+    const [newTodoValue, setNewTodoValue] = React.useState('');
+
+    const onSubmit = (event) => {
+        event.preventDefault();
+        addTodo(newTodoValue);
+        setOpenModal(false);
+    };
+
+    const onCancel = () => {
+        setOpenModal(false);
+    };
+
+    const onChange = (event) => {
+        setNewTodoValue(event.target.value);
+    };
+
     return (
-        <form onSubmit={(event) => {
-            event.preventDefault();
-        }}>
+        <form onSubmit={onSubmit}>
             <label>Escribe tu nuevo ToDo</label>
             <textarea
-                placeholder='Cortar Cebolla para el almuerzo'z/>
+                placeholder='Cortar Cebolla para el almuerzo'
+                value={newTodoValue}
+                onChange={onChange}/>
 
 
             <div className="TodoForm-buttonContainer">
                 <button 
+                    type="button"
                     className="TodoForm-button
-                    TodoForm-button--cancel">
+                    TodoForm-button--cancel"
+                    onClick={onCancel}
+                    >
                         Cancelar
                     </button>
                 <button 
+                    type="submit"
                     className="TodoForm-button
-                    TodoForm-button--add">
+                    TodoForm-button--add"
+                    onClick={onSubmit}>
                     Añadir
                     </button>
                 
